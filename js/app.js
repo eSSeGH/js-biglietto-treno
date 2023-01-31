02ef1fd2971cfdb02c85e4db4ace3a6420a65ff0
eSSeGH <ste.virgadaula@gmail.com>
2023-01-31 17:47:58 +0100

    EDIT+STYLE main added +JS variables html injection

--- js/app.js
+++ js/app.js
@@ -1,31 +1,48 @@
 // chiedere il nr dei chilometri da percorrere
+// e inserirlo nell'html
+
+let name = prompt("Inserire nome e cognome separati da uno spazio")
+console.log(name)
+
+const nameValue = document.getElementById('name')
+nameValue.innerHTML = name
+
+// chiedere il nr dei chilometri da percorrere
+// e inserirlo nell'html
 
 const km = parseInt(prompt("Quanti chilometri deve percorrere?"))
+
+const kmValue = document.getElementById('km')
+kmValue.innerHTML = km + "km"
 
 // chiedere età passeggero
 
 const età = parseInt(prompt("Qual'è la sua età?"))
 
-// calcolare prezzo del biglietto (km * 0.21 €/km)
+// calcolare prezzo del biglietto (km * 0.21 €/km) 
 
-let prezzo = (km * 0.21)
-console.log(prezzo)
+let price = (km * 0.21)
+console.log(price)
 
 // sconto 20% per età < 18 anni e sconto 40% per età > 65 anni
 
 if (età < 18) {
-    prezzo = (km * 0.21 * 0.8)
+    price = (km * 0.21 * 0.8)
 
 } else if (età > 65) {
-    prezzo = (km * 0.21 * 0.6)
+    price = (km * 0.21 * 0.6)
 
 } else {
-    prezzo = (km * 0.21)
+    price = (km * 0.21)
 
 }
-console.log(prezzo)
+console.log(price)
 
 // output con massimo di 2 decimali
+// e inserirlo nell'html
 
-prezzo = prezzo.toFixed(2)
-console.log(prezzo)
+price = price.toFixed(2)
+console.log(price)
+
+const priceValue = document.getElementById('price')
+priceValue.innerHTML = price + "€"
