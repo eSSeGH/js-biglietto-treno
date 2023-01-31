d1abcea62d87c6d0ca6a4a1d31d9336dfbe6cfc3
eSSeGH <ste.virgadaula@gmail.com>
2023-01-31 18:05:20 +0100

    BONUS exercise added

---
+++ bonus/js/app.js
@@ -0,0 +1,64 @@
+// chiedere il nr dei chilometri da percorrere
+// e inserirlo nell'html
+
+let name = prompt("Inserire nome e cognome separati da uno spazio")
+console.log(name)
+
+const nameValue = document.getElementById('name')
+nameValue.innerHTML = name
+
+// chiedere il nr dei chilometri da percorrere
+// e inserirlo nell'html
+
+const km = parseInt(prompt("Quanti chilometri deve percorrere?"))
+
+const kmValue = document.getElementById('km')
+kmValue.innerHTML = km + "km"
+
+// chiedere età passeggero
+
+const età = parseInt(prompt("Qual'è la sua età?"))
+
+// calcolare prezzo base del biglietto (km * 0.21 €/km) 
+// e inserirlo nell'html
+
+let basicPrice = (km * 0.21)
+console.log(basicPrice)
+
+const basicPriceValue = document.getElementById('basic-price')
+basicPriceValue.innerHTML = basicPrice + "€"
+
+// sconto 20% per età < 18 anni e sconto 40% per età > 65 anni
+
+let price = 0
+
+if (età < 18) {
+   price = (km * 0.21 * 0.8)
+
+} else if (età > 65) {
+    price = (km * 0.21 * 0.6)
+
+} else {
+    price = (km * 0.21)
+
+}
+console.log(price)
+
+// output con massimo di 2 decimali
+// e inserirlo nell'html
+
+price = price.toFixed(2)
+console.log(price)
+
+const priceValue = document.getElementById('price')
+priceValue.innerHTML = price + "€"
+
+// calcolare lo sconto e inserirlo nell'html
+
+const discount = -(price - basicPrice)
+console.log(discount)
+
+const discountValue = document.getElementById('discount')
+discountValue.innerHTML = discount + "€"
+
+
